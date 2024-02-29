86acac13561a1e9a427cffa567324c6e3b1b35a3
Fabio Santos <fabio_santos_493@hotmail.com>
2024-02-29 09:02:59 -0300

    Refatoracao

---
+++ ApiCatalogo/ApiCatalogo/AppServicesExtensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace ApiCatalogo.AppServicesExtensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app, 
+            IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            return app;
+        }
+
+        public static IApplicationBuilder UseAppCors(this IApplicationBuilder app)
+        {
+
+            string[] origens =
+          {
+               "http://192.168.0.157",
+                "http://192.168.0.95:5501",
+            };
+
+            app.UseCors(p =>
+            {
+                p.WithOrigins(origens);
+                p.WithMethods("GET","POST","PUT","DELETE","OPTIONS");
+                p.AllowAnyHeader();
+            });
+            return app;
+        }
+        public static IApplicationBuilder UseSwaggerMiddleware(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { });
+            return app;
+        }
+    }
+}
---
+++ ApiCatalogo/ApiCatalogo/Program.cs
@@ -0,0 +1,25 @@
+using ApiCatalogo.ApiEndpoints;
+using ApiCatalogo.AppServicesExtensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
+builder.AddAutenticationJwt();
+
+var app = builder.Build();
+
+app.MapAutenticacaoEndpoints();
+app.MapCategoriasEndpoints();
+app.MapProdutosEndpoints();
+
+var environment = app.Environment;
+app.UseExceptionHandling(environment)
+    .UseSwaggerMiddleware()
+    .UseAppCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
